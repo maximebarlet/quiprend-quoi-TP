fc143ec495368c609e2ed001c4a7d8afca975d25
Professionel <maximebarlet.pro@gmail.com>
2020-03-16 23:53:57 +0100

    04 - Rafraîchissement automatique

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Bienvenue');
+  res.send('Bonjour');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
