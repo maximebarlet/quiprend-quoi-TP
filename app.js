9fec8cf5afe4f0b0bffad30031bd9fad1b80746b
Professionel <maximebarlet.pro@gmail.com>
2020-03-19 16:12:09 +0100

    10 - La route POST

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
   });
 
+app.post('/party', function(req, res) {
+  res.send('Post ok !');
+  });
+
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
 
 app.set('view engine', 'pug');
