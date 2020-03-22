e612853336c016cbd1e33363320cdb4da5f4f76b
Professionel <maximebarlet.pro@gmail.com>
2020-03-22 18:51:06 +0100

    27 - Effectuer la copie

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -3,8 +3,23 @@ if (navigator.clipboard) {
         const $button = document.createElement('button');
         $button.innerHTML = 'Copier';
         $clipboardEl.parentNode.append($button);
+
+        $button.addEventListener(
+            'click',
+            copyToClipboard.bind(this, $clipboardEl, $button)
+          );
       });
     console.log("Support du presse papier")
   } else {
     console.warn("Pas de support")
+  }
+
+  function copyToClipboard($clipboardEl, $button) {
+    navigator.clipboard
+        .writeText($clipboardEl.getAttribute('data-clipboard'))
+        .then(() => {
+          $button.innerHTML = 'Copié !';
+          setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+        })
+        .catch((err) => console.warn(err));
   }
