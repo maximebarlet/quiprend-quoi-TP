52197128ebe8cc424ff394f08c8fa6944f54ab86
Professionel <maximebarlet.pro@gmail.com>
2020-03-22 19:19:38 +0100

    33 - Intercepter une requête

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 const name = 'mbarlet';
 console.log(`Bonjour ${name}`);
+
 addEventListener('install', (event) => {
     console.log('Hello from the service worker')
   });
+
+addEventListener('fetch', (event) => {
+if (event.request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(fetch(event.request));
+}
+});
