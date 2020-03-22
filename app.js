f3785c14434de3e6246e82c4b8ecfc45479950dd
Professionel <maximebarlet.pro@gmail.com>
2020-03-22 19:27:20 +0100

    34 - Page offline

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.static('pwa'));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
--- sw.js
+++ sw.js
@@ -3,10 +3,17 @@ console.log(`Bonjour ${name}`);
 
 addEventListener('install', (event) => {
     console.log('Hello from the service worker')
+    event.waitUntil(
+        caches.open('offline').then((cache) => {
+          cache.add('offline.html');
+        })
+      );
   });
 
 addEventListener('fetch', (event) => {
 if (event.request.headers.get('Accept').includes('text/html')) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match('offline.html')),
+      );
 }
 });
